6fdeee20dacb813c3e996ce0b2c052b49fa168b6
oneren37 <oneren37@gmail.com>
2023-02-28 11:41:10 +0300

    event emitter

--- src/game/EventEmitter.js
+++ src/game/EventEmitter.js
@@ -1,6 +1,6 @@
 export default class EventEmitter {
     /**
-     * @type {Object<function>}
+     * @type {Object<Set<function>>}
      */
     #events = {}
 
@@ -9,19 +9,28 @@ export default class EventEmitter {
      * @param {string} eventName 
      * @param {function} fn 
      */
-    on(eventName, fn) {}
+    on(eventName, fn) {
+        this.#events[eventName] = this.#events[eventName] || new Set()
+        this.#events[eventName].add(fn)
+    }
 
     /**
      * Отписаться от события
      * @param {string} eventName 
      * @param {function} fn 
      */
-    unsubscribe(eventName, fn) {}
+    unsubscribe(eventName, fn) {
+        if (!this.#events[eventName]) return 
+        this.#events[eventName].delete(fn)
+    }
 
     /**
      * Вызвать событие
      * @param {string} eventName 
      * @param {any} data 
      */
-    emit(eventName, data) {}
+    emit(eventName, data) {
+        if (!this.#events[eventName]) return 
+        this.#events[eventName].forEach(fn => fn(data))
+    }
 }
